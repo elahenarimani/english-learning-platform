e10ba6ee11f009235f59c84ef9681453b27b6de1
Elahe Narimani <elahe.narimani@gmail.com>
2026-09-03 16:23:44 +0330

    add styles

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { ThemeToggle } from "@/components/ui/theme-toggle/ThemeToggle";
 import { useTranslations } from "next-intl";
-import { LanguageSwitcher } from "./LanguageSwitcher";
+import { LanguageSwitcher } from "../LanguageSwitcher";
+import styles from "./Navbar.module.scss"
 export function Navbar() {
   const t = useTranslations("Home");
   return (
-    <nav>
+    <nav  className={styles.navbar}>
       <h1 className="dark:text-white dark:bg-amber-700">{t("title")}</h1>
       <p>{t("description")}</p>
       <ThemeToggle />
